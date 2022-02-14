bae5d0123bc404d05afa0722b8d42d4199d6ecb2
SnezhanaKorekova <94750455+SnezhanaKorekova@users.noreply.github.com>
2022-02-14 10:10:45 +0300

    Add files via upload

--- s.py
+++ s.py
@@ -1,13 +1,17 @@
 import requests
 
 
-def picture(coord_x, coord_y, scale):
-    '''coord_x, coord_y = map(float, input('Введите координаты (формат ввода: x,y): ').split(','))
-    scale_x, scale_y = map(float, input('Введите масштаб (формат ввода: x,y): ').split(','))'''
+def picture(coord_x, coord_y, scale, space='map'):
+    if -179 <= coord_x <= 179 and -79 <=coord_y <= 79 and 0 < scale < 90:
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord_x},{coord_y}&spn={scale},{scale}&l={space}"
+        response = requests.get(map_request)
 
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord_x},{coord_y}&spn={scale},{scale}&l=map"
-    response = requests.get(map_request)
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    else:
+        print('ERROR')
+        coord_x, coord_y = map(float, input('Введите координаты (формат ввода: x,y): ').split(','))
+        scale = float(input('Введите масштаб (формат ввода: x): '))
+        picture(coord_x, coord_y, scale)
 
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
