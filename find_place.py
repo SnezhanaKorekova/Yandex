5bd21389c1edcc1926131e5d91a62a0dd380278a
SnezhanaKorekova <94750455+SnezhanaKorekova@users.noreply.github.com>
2022-02-15 18:16:46 +0300

    Add files via upload

---
+++ find_place.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def find_place(toponim_to_find):
+    toponim_to_find = '+'.join(toponim_to_find.split())
+
+    server_gtocoder = 'http://geocode-maps.yandex.ru/1.x/'
+    params = {'apikey': "40d1649f-0493-4b70-98ba-98533de7710b",
+              'geocode': toponim_to_find,
+              'format': 'json'}
+
+    response_geocoder = requests.get(server_gtocoder, params=params)
+    json_response = response_geocoder.json()
+
+    pos = json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+    coord_x, coord_y = pos.split()
+
+    return float(coord_x), float(coord_y)
